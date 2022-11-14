d5f017f16df3ae9425276254fd53cd1153116271
Yannick Schini <yannick.schini@gmail.com>
2022-11-14 09:28:32 +0100

    fix: Correct the icons on the messages

--- .config/nvim/after/plugin/alpha.lua
+++ .config/nvim/after/plugin/alpha.lua
@@ -31,8 +31,8 @@ dashboard.section.header.val = {
 -- Set menu
 dashboard.section.buttons.val = {
     dashboard.button( "e", "  > New file" , ":ene<CR>"),
-    dashboard.button( "<leader>gf", "  > Git file", ":Telescope git_files<CR>"),
-    dashboard.button( "<leader>ff", "  > Find file", ":Telescope find_files<CR>"),
+    dashboard.button( "<leader>gf", "  > Git file", ":Telescope git_files<CR>"),
+    dashboard.button( "<leader>ff", "  > Find file", ":Telescope find_files<CR>"),
     dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
     dashboard.button( "v", "  > Vim Config" , ":cd ~/Documents/work_station_config | Telescope find_files<CR>"),
     dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
