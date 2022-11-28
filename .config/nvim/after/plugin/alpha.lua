7e2336be0768e762c6f3c5c1f2ae64f56b130723
Yannick Schini <yannick.schini@backmarket.com>
2022-11-28 08:21:08 +0100

    feat(nvim): Add shortcut to taking notes

--- .config/nvim/after/plugin/alpha.lua
+++ .config/nvim/after/plugin/alpha.lua
@@ -35,6 +35,7 @@ dashboard.section.buttons.val = {
     dashboard.button( "<leader>ff", "  > Find file", ":Telescope find_files<CR>"),
     dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
     dashboard.button( "v", "  > Vim Config" , ":cd ~/Documents/work_station_config | Telescope find_files<CR>"),
+    dashboard.button( "n", "  > Notes" , ":cd ~/Documents/notes | Telescope find_files<CR>"),
     dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
 }
 
