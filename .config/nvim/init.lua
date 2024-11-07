c23945dacdae59ed7a699ea8484d09566e5bb5e2
Yannick Schini <yannick.schini@backmarket.com>
2024-11-07 11:33:53 +0100

    feat: Add keymaps to navigate through buffers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,3 +3,42 @@ if Bepo then require("config.bepo_movement") end
 require("config")
 
 vim.cmd("colorscheme tokyonight")
+
+local cur_buf = vim.api.nvim_get_current_buf
+local set_buf = vim.api.nvim_set_current_buf
+
+local function buf_index(bufnr)
+    local bufs = vim.api.nvim_list_bufs()
+    for i, value in ipairs(bufs) do
+        if value == bufnr then
+            return i
+        end
+    end
+end
+
+local function next_buf()
+    local bufs = vim.api.nvim_list_bufs()
+    local curbufIndex = buf_index(cur_buf())
+
+    if not curbufIndex then
+        set_buf(bufs[1])
+        return
+    end
+
+    set_buf((curbufIndex == #bufs and bufs[1]) or bufs[curbufIndex + 1])
+end
+
+local function prev_buf()
+    local bufs = vim.api.nvim_list_bufs()
+    local curbufIndex = buf_index(cur_buf())
+
+    if not curbufIndex then
+        set_buf(bufs[1])
+        return
+    end
+
+    set_buf((curbufIndex == 1 and bufs[#bufs]) or bufs[curbufIndex - 1])
+end
+
+vim.keymap.set("n", "<TAB>", function() next_buf() end, { desc = "buffer goto next" })
+vim.keymap.set("n", "<S-TAB>", function() prev_buf() end, { desc = "buffer goto prev" })
