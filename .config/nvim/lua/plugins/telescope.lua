19ccb3914e672417ce80511793ab664ac50e480f
Yannick Schini <yannick.schini@backmarket.com>
2024-11-07 11:31:09 +0100

    feat(Telescope): Search now includes hidden files but not .git folder !

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,15 +7,32 @@ return {
         local telescope = require("telescope")
 
         telescope.setup({
+            defaults = {
+                vimgrep_arguments = {
+                    "rg",
+                    "--follow",        -- Follow symbolic links
+                    "--hidden",        -- Search for hidden files
+                    "--no-heading",    -- Don't group matches by each file
+                    "--with-filename", -- Print the file path with the matched lines
+                    "--line-number",   -- Show line numbers
+                    "--column",        -- Show column numbers
+                    "--smart-case",    -- Smart case search,
+                },
+            },
             pickers = {
                 buffers = {
                     show_all_buffers = true,
                     sort_lastused = true,
                 },
-            },
-            find_files = {
-                -- `hidden = true` will still show the inside of `.git/` as it's not ignored in the `.gitignore` file.
-                find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" }
+                find_files = {
+                    hidden = true,
+                    find_command = {
+                        "rg",
+                        "--files",          -- Search for files (respects .gitignore by default)
+                        "--hidden",         -- Include hidden files
+                        "--glob=!**/.git/*" -- Do not search for files in the .git folder
+                    },
+                },
             },
         })
     end,
