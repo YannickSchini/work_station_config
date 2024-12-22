24a6b72f7b44d3daa25e1e6989adf71519c59f45
Yannick Schini <yannick.schini@backmarket.com>
2024-12-22 21:19:27 +0100

    feat: Add kdl treesitter grammar

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,28 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			auto_install = true,
-			ensure_installed = {
-				"lua",
-				"hcl",
-				"markdown",
-				"dockerfile",
-				"json",
-				"toml",
-				"make",
-				"markdown_inline",
-				"python",
-				"terraform",
-				"yaml",
-				"bash",
-			},
-			sync_install = false,
-			highlight = { enable = true },
-		})
-	end,
+        configs.setup({
+            auto_install = true,
+            ensure_installed = {
+                "lua",
+                "hcl",
+                "markdown",
+                "dockerfile",
+                "json",
+                "toml",
+                "make",
+                "markdown_inline",
+                "python",
+                "terraform",
+                "yaml",
+                "bash",
+                "kdl",
+            },
+            sync_install = false,
+            highlight = { enable = true },
+        })
+    end,
 }
