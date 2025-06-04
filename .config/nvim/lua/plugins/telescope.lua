c62e97b0a22040686b55ebc15126f800035759e6
Yannick Schini <yannick.schini@backmarket.com>
2025-06-04 16:54:50 +0200

    fix: Exclude Git files from vimgrep results

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -10,13 +10,14 @@ return {
             defaults = {
                 vimgrep_arguments = {
                     "rg",
-                    "--follow",        -- Follow symbolic links
-                    "--hidden",        -- Search for hidden files
-                    "--no-heading",    -- Don't group matches by each file
-                    "--with-filename", -- Print the file path with the matched lines
-                    "--line-number",   -- Show line numbers
-                    "--column",        -- Show column numbers
-                    "--smart-case",    -- Smart case search,
+                    "--follow",         -- Follow symbolic links
+                    "--hidden",         -- Search for hidden files
+                    "--no-heading",     -- Don't group matches by each file
+                    "--with-filename",  -- Print the file path with the matched lines
+                    "--line-number",    -- Show line numbers
+                    "--column",         -- Show column numbers
+                    "--smart-case",     -- Smart case search,
+                    "--glob=!**/.git/*" -- Do not search for files in the .git folder
                 },
             },
             pickers = {
